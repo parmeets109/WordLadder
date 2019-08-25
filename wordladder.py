b3b29d706432aca331791e16336447fb221c0123
parmeets109 <54392833+parmeets109@users.noreply.github.com>
2019-08-25 20:26:36 +1000

    Commit3

--- wordladder.py
+++ wordladder.py
@@ -54,3 +54,48 @@ def targetWordCheck(word):
         elif word.isalpha():  # checks if word is alpha then returns the word
             word.replace(" ", "")
             return word
+# This function is used to check the word that must exist on the path
+def MustWordCheck(word):
+    global start
+    while True:
+        if word.isdigit():  # checks that if the input is digit then program displays an error message
+            print("Word should not be a numeric.")
+            word = input("Please enter a valid word.\n:")
+        elif len(word) !=0 and len(word) != len(start):  # checks that target word must of same length as start word. otherwise displays an error message
+            print("Necessary Word must be of same length as start word.")
+            word = input("Please enter valid word\n:")
+        else:
+            return word
+
+
+# This function is used to create a list of words that should not exist in the path
+def blacklisted(words):
+    blacklist_words = words.split(',')
+    return blacklist_words
+
+
+# This function is used to provide a path selection for the user
+def pathSelection(path):
+    path.lower()
+    while True:
+        if path.isdigit():
+            print(" Invalid")
+            path = input("please select either longer(L) or shorter(S) ").lower()
+        elif path == "":
+            print("Invalid")
+            path = input("please select either longer(L) or shorter(S)").lower()
+
+        else:
+            if path == 'l':
+                return False
+            elif path == 's':
+                return True
+            else:
+                path = input("please select either longer(L) or shorter(S)").lower()
+                continue
+
+
+#  This function is used to  returns a number of letters and indexes for two words that  exactly same
+def same(item, target):
+    return len([item for (item, target) in zip(item, target) if item == target])
+
